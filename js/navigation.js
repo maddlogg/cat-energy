1719c5350a43b793619c054a51879d3a329a87be
maddlogg <marshalmatthers97@gmail.com>
2024-05-27 23:05:25 +0300

    Made PP for index mobile version

--- js/navigation.js
+++ js/navigation.js
@@ -1,6 +1,7 @@
 let buttonMenu = document.querySelector(".main-navigation__toggle");
 let mainNav = document.querySelector(".main-navigation");
 
+mainNav.classList.remove("main-navigation--nojs");
 mainNav.classList.add("main-navigation--closed");
 
 buttonMenu.addEventListener("click", function () {
@@ -10,5 +11,15 @@ buttonMenu.addEventListener("click", function () {
   } else {
     mainNav.classList.remove("main-navigation--opened");
     mainNav.classList.add("main-navigation--closed");
+  }
+});
+
+window.addEventListener("resize", function () {
+  if (
+    window.matchMedia("(min-width: 768px)").matches &&
+    mainNav.classList.contains("main-navigation--closed")
+  ) {
+    mainNav.classList.remove("main-navigation--closed");
+    mainNav.classList.add("main-navigation--opened");
   }
 });

dfab619c7896001f85444ac04acf3879e4d15c7a
maddlogg <marshalmatthers97@gmail.com>
2024-06-13 23:20:16 +0300

    Minor updates for index, started catalog mobile version

--- js/navigation.js
+++ js/navigation.js
@@ -2,7 +2,14 @@ let buttonMenu = document.querySelector(".main-navigation__toggle");
 let mainNav = document.querySelector(".main-navigation");
 
 mainNav.classList.remove("main-navigation--nojs");
-mainNav.classList.add("main-navigation--closed");
+
+if (
+  window.matchMedia("(min-width: 768px)").matches &&
+  mainNav.classList.contains("main-navigation--closed")
+) {
+  mainNav.classList.remove("main-navigation--closed");
+  mainNav.classList.add("main-navigation--opened");
+}
 
 buttonMenu.addEventListener("click", function () {
   if (mainNav.classList.contains("main-navigation--closed")) {

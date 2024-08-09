70044a28e2664a688ab37ffe195dcd5c2c39019d
maddlogg <marshalmatthers97@gmail.com>
2024-08-09 19:48:52 +0300

    Finished project's assembly with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 const { watch } = require("gulp");
 const postcss = require("gulp-postcss");
+const posthtml = require("gulp-posthtml");
 const autoprefixer = require("autoprefixer");
+const cssmin = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = import("gulp-imagemin");
+const webp = import("gulp-webp");
 const sourcemaps = require("gulp-sourcemaps");
+const server = require("browser-sync").create();
 const { run } = require("node:test");
+const del = require("del");
+const browserSync = require("browser-sync");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify-es").default;
+const babel = require("gulp-babel");
 
 gulp.task("styles", function () {
   return gulp
-    .src("./sass/**/styles.scss")
+    .src("source/sass/styles.scss")
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
     .pipe(
@@ -19,11 +32,83 @@ gulp.task("styles", function () {
       ])
     )
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("./css"));
+    .pipe(gulp.dest("build/css"))
+    .pipe(cssmin())
+    .pipe(rename("styles.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
 });
 
-exports.watch = function () {
-  gulp.watch("./sass/*.scss", gulp.series("styles"));
-  gulp.watch("./sass/blocks/*.scss", gulp.series("styles"));
-  gulp.watch("./sass/variables/*.scss", gulp.series("styles"));
-};
+gulp.task("serve", function () {
+  server.init({
+    server: "build/",
+    reloadDelay: 200,
+  });
+
+  gulp.watch("source/sass/*.scss", gulp.series("styles"));
+  gulp.watch("source/sass/blocks/*.scss", gulp.series("styles"));
+  gulp.watch("source/sass/variables/*.scss", gulp.series("styles"));
+  gulp.watch("source/*.html").on("change", gulp.series("html", server.reload));
+  gulp.watch(["source/**/*.js", "!source/**/*.min.js"], gulp.series("scripts"));
+});
+
+gulp.task("images", function () {
+  return gulp
+    .src("source/img/**/*.{png,jpg,svg}")
+    .pipe(
+      imagemin([
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.svgo(),
+      ])
+    )
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp
+    .src("source/img/**/*.{.png, jpg}")
+    .pipe(webp({ quality: 90 }))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*html").pipe(posthtml()).pipe(gulp.dest("build"));
+});
+
+gulp.task("scripts", function () {
+  return gulp
+    .src(["source/js/**/*.js"])
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"],
+      })
+    )
+    .pipe(concat("scripts.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("build/js/"))
+    .pipe(server.stream());
+});
+
+gulp.task("copy", function () {
+  return gulp
+    .src(["source/fonts/**/*.ttf", "source/img/**", "source/js/**/*.min.js"], {
+      base: "source",
+      buffer: true,
+      removeBOM: false,
+      encoding: false,
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del(["build"]);
+});
+
+gulp.task("build", gulp.series("clean", "copy", "styles", "html"));
+
+// exports.watch = function () {
+//   gulp.watch("./sass/*.scss", gulp.series("styles"));
+//   gulp.watch("./sass/blocks/*.scss", gulp.series("styles"));
+//   gulp.watch("./sass/variables/*.scss", gulp.series("styles"));
+// };
